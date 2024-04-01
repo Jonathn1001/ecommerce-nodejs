5b5b02f70209b89ffee296c3fbdf4c1f68fc2a63
Robert lee <sangle1001.dev@gmail.com>
2024-04-02 01:36:18 +0700

    remove console log

--- src/app.js
+++ src/app.js
@@ -2,14 +2,12 @@ const compression = require("compression");
 const express = require("express");
 const { default: helmet } = require("helmet");
 const morgan = require("morgan");
-const path = require("path");
+
 const dotenv = require("dotenv");
 
 dotenv.config({
-  // path: path.join(__dirname, `./.env.${process.env.NODE_ENV}`),
   path: `./.env.${process.env.NODE_ENV}`,
 });
-console.log(path.join(__dirname, `./.env.${process.env.NODE_ENV}`));
 
 const app = express();
 
